7198894f3f1def5f8d74b9a95fa59bbda80b9df1
krisoubre <kristoferoubre@yahoo.com>
2017-03-29 13:05:31 -0500

    Added Music Player Script
    Added Music Player script.

---
+++ GrappleGame/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,38 @@
+﻿// This script was written by Kristofer Oubre for the
+// game Flee Bee © Kristofer Oubre which is available
+// in the Apple App Store and Google Play Store. Some 
+// functions will need to be tweaked so this script 
+// can be useful in other games.
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+	public AudioSource music;
+	private static bool AudioBegin = false;
+
+	void Start ()
+	{
+		music = GetComponent<AudioSource> ();
+	}
+
+	void Awake ()
+	{
+		if (!AudioBegin) {
+			music.Play ();
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (SceneManager.GetActiveScene ().name == "Game" || SceneManager.GetActiveScene ().name == "GameOver") {
+			music.Stop ();
+			AudioBegin = false;
+		}
+	}
+}

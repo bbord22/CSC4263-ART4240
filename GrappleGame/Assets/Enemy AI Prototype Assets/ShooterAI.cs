c988d21f3532f1397d36aff4f32ae1bbab3e45e8
krisoubre <kristoferoubre@yahoo.com>
2017-04-24 21:57:05 -0500

    Added Main Menu and Game Over Scenes

--- GrappleGame/Assets/Enemy AI Prototype Assets/ShooterAI.cs
+++ GrappleGame/Assets/Enemy AI Prototype Assets/ShooterAI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShooterAI : MonoBehaviour
 {
@@ -69,8 +70,14 @@ public class ShooterAI : MonoBehaviour
             player = null;
             playerT = null;
             Destroy(other.gameObject);
+			StartCoroutine ("EndGame");
         }
     }
+
+	IEnumerator EndGame(){
+		yield return new WaitForSeconds (3);
+		SceneManager.LoadScene ("Scenes/GameOver");
+	}
 
     void shoot()
     {
